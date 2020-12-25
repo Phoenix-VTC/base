9b3435d33c3737b83f8465bf866859efb9d55ff3
Diego <me@diegor.nl>
2020-12-25 02:35:04 +0100

    Refactor driver application views

--- resources/views/layouts/driver-application.blade.php
+++ resources/views/layouts/driver-application.blade.php
@@ -2,7 +2,66 @@
 
 @section('body')
     <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-900 sm:px-6 lg:px-8">
-        @yield('content')
+        <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="bg-gray-50 overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+                <div class="px-4 py-5 sm:px-6">
+                    @include('livewire.driver-application.components.steps', ['current' => 2])
+                </div>
+                <div class="px-4 py-5 sm:p-6 pb-5 min-h-96">
+                    @if ($errors->any())
+                        <div class="rounded-md bg-red-200 p-4 mb-5">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd"
+                                              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                              clip-rule="evenodd"/>
+                                    </svg>
+                                </div>
+                                <div class="ml-3">
+                                    <h3 class="text-sm font-medium text-red-800">
+                                        <strong>Whoops, something went wrong!</strong>
+                                        <br>
+                                        Please check the errors and try again.
+                                    </h3>
+                                    <div class="mt-2 text-sm text-red-800">
+                                        <ul class="list-disc pl-5 space-y-1">
+                                            @foreach ($errors->all() as $error)
+                                                <li>{!! $error !!}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @yield('content')
+                </div>
+                @if(session()->has('steam_user'))
+                    <div class="px-4 py-5 sm:p-6">
+                        <div class="flex-shrink-0 group block">
+                            <div class="flex items-center">
+                                <a href="{{ session('steam_user.profileurl') }}" target="_blank">
+                                    <img class="inline-block h-9 w-9 rounded-full"
+                                         src="{{ session('steam_user.avatar') }}"
+                                         alt="{{ session('steam_user.personaname') }}">
+                                </a>
+                                <div class="ml-3">
+                                    <p class="text-sm font-bold text-gray-700 group-hover:text-gray-900">
+                                        <a href="{{ session('steam_user.profileurl') }}"
+                                           target="_blank">{{ session('steam_user.personaname') }}</a>
+                                    </p>
+                                    <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700" href="#">
+                                        <a href="{{ route('driver-application.auth.steam.logout') }}">Log out</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
 
         @isset($slot)
             {{ $slot }}
--- resources/views/livewire/driver-application/auth.blade.php
+++ resources/views/livewire/driver-application/auth.blade.php
@@ -1,46 +1,19 @@
-<div class="container mx-auto px-4 sm:px-6 lg:px-8">
-    <div class="bg-gray-50 overflow-hidden shadow rounded-lg divide-y divide-gray-200">
-        <div class="px-4 py-5 sm:px-6">
-            @include('livewire.driver-application.components.steps', ['current' => 2])
-        </div>
-        <div class="px-4 py-5 sm:p-6 pb-5 h-96 text-center">
-            <h3 class="text-2xl leading-6 font-bold text-gray-900 pb-5">
-                Log in with Steam
-            </h3>
-            <img class="object-contain h-48 w-full pb-5" src="{{ asset('img/illustrations/hire.svg') }}"
-                 alt="Illustration"/>
-            <a class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-               href="{{ route('driver-application.auth.steam') }}">
-                <svg class="-ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                     fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd"
-                          d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
-                          clip-rule="evenodd"/>
-                </svg>
-                Authenticate
-            </a>
-        </div>
-        <div class="px-4 py-5 sm:p-6">
-            @if(session()->has('steam_user'))
-                <div class="flex-shrink-0 group block">
-                    <div class="flex items-center">
-                        <a href="{{ session('steam_user.profileurl') }}" target="_blank">
-                            <img class="inline-block h-9 w-9 rounded-full"
-                                 src="{{ session('steam_user.avatar') }}"
-                                 alt="{{ session('steam_user.personaname') }}">
-                        </a>
-                        <div class="ml-3">
-                            <p class="text-sm font-bold text-gray-700 group-hover:text-gray-900">
-                                <a href="{{ session('steam_user.profileurl') }}"
-                                   target="_blank">{{ session('steam_user.personaname') }}</a>
-                            </p>
-                            <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700" href="#">
-                                <a href="{{ route('driver-application.auth.steam.logout') }}">Log out</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            @endif
-        </div>
-    </div>
-</div>
+<form class="text-center" action="{{ route('driver-application.auth.steam') }}" method="POST">
+    @csrf
+    <h3 class="text-2xl leading-6 font-bold text-gray-900 pb-5">
+        Log in with Steam
+    </h3>
+    <img class="object-contain h-48 w-full pb-5" src="{{ asset('img/illustrations/hire.svg') }}"
+         alt="Illustration"/>
+    <button
+        class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+        type="submit">
+        <svg class="-ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+             fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd"
+                  d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                  clip-rule="evenodd"/>
+        </svg>
+        Authenticate
+    </button>
+</form>
