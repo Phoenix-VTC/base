8b8404d6c6f51240c0b52c045be5fdfdc407add6
Diego <me@diegor.nl>
2020-12-27 18:46:14 +0100

    Update steps placeholder text

--- resources/views/livewire/driver-application/components/steps.blade.php
+++ resources/views/livewire/driver-application/components/steps.blade.php
@@ -5,7 +5,7 @@
             <a href="#"
                class="group pl-4 py-2 flex flex-col border-l-4 border-red-600 hover:border-red-800 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4">
                 <span class="text-xs text-red-600 font-semibold uppercase group-hover:text-red-800">Step 1</span>
-                <span class="text-sm font-medium">Job details</span>
+                <span class="text-sm font-medium">Personal Information</span>
             </a>
         </li>
 
@@ -15,7 +15,7 @@
                class="pl-4 py-2 flex flex-col border-l-4 border-red-600 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
                aria-current="step">
                 <span class="text-xs text-red-600 font-semibold uppercase">Step 2</span>
-                <span class="text-sm font-medium">Application form</span>
+                <span class="text-sm font-medium">Digital Interview</span>
             </a>
         </li>
 
@@ -25,7 +25,7 @@
                class="group pl-4 py-2 flex flex-col border-l-4 border-gray-200 hover:border-gray-300 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4">
                             <span
                                 class="text-xs text-gray-500 font-semibold uppercase group-hover:text-gray-700">Step 3</span>
-                <span class="text-sm font-medium">Preview</span>
+                <span class="text-sm font-medium">Confirmation</span>
             </a>
         </li>
     </ol>
