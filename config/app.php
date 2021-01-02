0bffceea3d8fa85567726ba5f6fc0a481b28d486
iDiegoNL <$JkPBploP^BZ9m4grlDK8>
2021-01-03 00:17:14 +0100

    Try to fix Horizon 403

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\HorizonServiceProvider::class,
 
         /*
          * Application Service Providers...
