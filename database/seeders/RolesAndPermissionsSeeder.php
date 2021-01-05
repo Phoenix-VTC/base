d19a4b90655e36850ce9a4c43536b8726a9f4615
Diego <me@diegor.nl>
2021-01-05 02:29:01 +0100

    Set up the super admin and recruiter roles & permissions

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create permissions
+        Permission::create(['name' => 'handle driver applications']);
+
+        // Create roles and assign created permissions
+        Role::create(['name' => 'recruiter'])
+            ->givePermissionTo(['handle driver applications']);
+
+        Role::create(['name' => 'super admin']);
+    }
+}
