4404d2e45c496b008207157c6b11f7ad4d752501
Diego <me@diegor.nl>
2021-01-05 16:20:13 +0100

    Create appliction factory & seeder

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Application;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ApplicationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Application::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function definition(): array
+    {
+        $steamData = [
+            "avatar" => $this->faker->imageUrl(),
+            "realname" => $this->faker->name,
+            "loccityid" => random_int(1, 99999),
+            "steamID64" => random_int(1000000, 10000000000000000),
+            "avatarfull" => $this->faker->imageUrl(),
+            "avatarhash" => $this->faker->md5,
+            "lastlogoff" => $this->faker->unixTime,
+            "profileurl" => "https://steamcommunity.com/id/" . $this->faker->userName,
+            "personaname" => $this->faker->userName,
+            "timecreated" => $this->faker->unixTime,
+            "avatarmedium" => $this->faker->imageUrl(),
+            "locstatecode" => random_int(0, 10),
+            "personastate" => random_int(0, 6),
+            "profilestate" => 1,
+            "primaryclanid" => random_int(1000000, 10000000000000000),
+            "loccountrycode" => $this->faker->countryCode,
+            "personastateflags" => 0,
+            "communityvisibilitystate" => 3,
+        ];
+
+        $application_answers = [];
+        foreach (__('driver-application.additional_questions') as $question) {
+            $answer = $this->faker->realText();
+
+            $application_answers[$question] = $answer;
+        }
+
+        return [
+            'username' => $this->faker->unique()->userName,
+            'email' => $this->faker->unique()->safeEmail,
+            'date_of_birth' => $this->faker->date(),
+            'country' => $this->faker->countryCode,
+            'steam_data' => json_encode($steamData, JSON_THROW_ON_ERROR),
+            'truckersmp_id' => 3181778,
+            'application_answers' => json_encode($application_answers, JSON_THROW_ON_ERROR),
+        ];
+    }
+}
