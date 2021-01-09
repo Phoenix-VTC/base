3dd2ea37ab3d77320d38fb649325ebdafc9e0214
Diego <me@diegor.nl>
2021-01-09 03:36:03 +0100

    Implement sign out functionality

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -71,9 +71,13 @@
                     {{--                        Settings--}}
                     {{--                    </a>--}}
 
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
-                        Sign out
-                    </a>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                role="menuitem">
+                            Sign out
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
