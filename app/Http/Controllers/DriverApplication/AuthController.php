3d66dc55478dc185ed742b71c590b197dc4ad818
Diego <me@diegor.nl>
2021-01-09 02:49:06 +0100

    Update validation rules for Matthew :)

--- app/Http/Controllers/DriverApplication/AuthController.php
+++ app/Http/Controllers/DriverApplication/AuthController.php
@@ -62,7 +62,7 @@ class AuthController extends Controller
                 $validator = Validator::make($info->toArray(), [
                     // Temporarily disabled the validation for testing purposes
 //                    'steamID64' => ['bail', new HasGame, new MinHours, new AccountExists, new BanHistoryPublic, new NoRecentBans, new NotInVTC],
-                    'steamID64' => ['bail', new HasGame, new MinHours, new AccountExists, new BanHistoryPublic, new NoRecentBans],
+                    'steamID64' => ['bail', new HasGame, new MinHours, new AccountExists, new BanHistoryPublic],
                 ]);
 
                 if ($validator->fails()) {
