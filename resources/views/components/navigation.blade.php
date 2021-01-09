83b7b1ffa351bdaec3e27f6d5960a127fabd787e
Diego <me@diegor.nl>
2021-01-09 03:34:08 +0100

    Comment out non-functional buttons

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -30,16 +30,16 @@
             </form>
         </div>
         <div class="ml-4 flex items-center md:ml-6">
-            <button
-                class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <span class="sr-only">View notifications</span>
-                {{-- bell --}}
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
-                </svg>
-            </button>
+            {{--            <button--}}
+            {{--                class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">--}}
+            {{--                <span class="sr-only">View notifications</span>--}}
+            {{--                --}}{{-- bell --}}
+            {{--                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"--}}
+            {{--                     stroke="currentColor" aria-hidden="true">--}}
+            {{--                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+            {{--                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>--}}
+            {{--                </svg>--}}
+            {{--            </button>--}}
 
             {{-- Profile Dropdown --}}
             <div class="ml-3 relative" x-data="{ profileOpen: false }">
@@ -63,13 +63,13 @@
                     x-transition:leave="transition ease-in duration-75"
                     x-transition:leave-start="transform opacity-100 scale-100"
                     x-transition:leave-end="transform opacity-0 scale-95">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
-                        Your Profile
-                    </a>
+                    {{--                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">--}}
+                    {{--                        Your Profile--}}
+                    {{--                    </a>--}}
 
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
-                        Settings
-                    </a>
+                    {{--                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">--}}
+                    {{--                        Settings--}}
+                    {{--                    </a>--}}
 
                     <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
                         Sign out
