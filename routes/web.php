064e3c16c379b51431e85ad97836ab7d63e1ae14
Diego <me@diegor.nl>
2021-01-09 13:01:22 +0100

    Update register route

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,9 @@ Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
         ->name('login');
 
-    Route::get('register', Register::class)
-        ->name('register');
+    Route::get('/dashboard', function () {
+        return redirect('https://apply.phoenixvtc.com');
+    })->name('register');
 });
 
 Route::get('password/reset', Email::class)
