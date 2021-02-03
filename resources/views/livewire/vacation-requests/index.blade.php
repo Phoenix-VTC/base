c7a1354b745e86104947a3b576b0c2c0fe3fabe4
Diego <me@diegor.nl>
2021-02-03 20:44:55 +0100

    Add cancelled label to status

--- resources/views/livewire/vacation-requests/index.blade.php
+++ resources/views/livewire/vacation-requests/index.blade.php
@@ -77,6 +77,11 @@
                                                     class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                                                     Pending
                                                 </span>
+                                            @elseif($vacation_request->deleted_at)
+                                                <span
+                                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                                    Cancelled
+                                                </span>
                                             @else
                                                 @if($vacation_request->is_upcoming)
                                                     <span
