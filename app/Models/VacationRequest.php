5a3bb654dc2e5d00498630add370522dbbafadae
Diego <me@diegor.nl>
2021-02-09 13:21:28 +0100

    Use Notifiable in the VacationRequest model

--- app/Models/VacationRequest.php
+++ app/Models/VacationRequest.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class VacationRequest extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Notifiable;
 
     protected $guarded = [];
 
@@ -43,5 +45,15 @@ class VacationRequest extends Model
     public function getDurationAttribute(): string
     {
         return Carbon::parse($this->end_date)->diffForHumans($this->start_date, true);
+    }
+
+    /**
+     * Route notifications for the Discord HR channel.
+     *
+     * @return string
+     */
+    public function routeNotificationForDiscord(): string
+    {
+        return config('services.discord.hr_channel_id');
     }
 }
