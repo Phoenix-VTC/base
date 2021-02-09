0e29aadb2bf5dea40118bb00936e614e69c9c320
iDiego <me@diegor.nl>
2021-02-09 14:06:56 +0100

    Include deleted items in the user management (#22)
    * Include deleted items in the user management [ch68]
    
    * Make username gray if account is deleted

--- resources/views/livewire/user-management/datatable-components/username-field.blade.php
+++ resources/views/livewire/user-management/datatable-components/username-field.blade.php
@@ -1,3 +1,4 @@
-<a href="#" class="border-2 border-transparent hover:border-blue-500 hover:bg-blue-100 hover:shadow-lg text-blue-600 rounded-lg px-3 py-1">
+<a href="#"
+   class="border-2 border-transparent @if(!$row->deleted_at) text-blue-600 @endif hover:border-blue-500 hover:bg-blue-100 hover:shadow-lg rounded-lg px-3 py-1">
     {!! $value !!}
 </a>
