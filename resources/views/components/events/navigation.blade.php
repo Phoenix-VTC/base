eb859b1ece3ac65767a2f966717565c0c24813a6
Diego <me@diegor.nl>
2021-02-11 01:48:09 +0100

    Rename hero-subtitle to hero-description [ch113]

--- resources/views/components/events/navigation.blade.php
+++ resources/views/components/events/navigation.blade.php
@@ -122,7 +122,7 @@
                         @yield('hero-title')
                     </h1>
                     <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                        @yield('hero-subtitle')
+                        @yield('hero-description')
                     </p>
                 </div>
             </div>
