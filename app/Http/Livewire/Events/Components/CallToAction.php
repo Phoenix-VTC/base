dcae212bb9f4c12ab4c0bbb08509696f5d4bd1ab
Diego <me@diegor.nl>
2021-02-11 13:51:30 +0100

    Create Call to Action component [ch113]

---
+++ app/Http/Livewire/Events/Components/CallToAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\Events\Components;
+
+use Livewire\Component;
+
+class CallToAction extends Component
+{
+    public string $tag;
+    public string $title;
+    public string $description;
+    public string $buttonText;
+    public string $buttonUrl;
+
+    public function render()
+    {
+        return view('livewire.events.components.call-to-action');
+    }
+}
