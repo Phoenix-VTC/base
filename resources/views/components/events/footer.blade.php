1f3ab61235dcefbe47af5d265b781fed02e0c64d
Diego <me@diegor.nl>
2021-03-05 13:53:23 +0100

    Add main website item to the footer nav

--- resources/views/components/events/footer.blade.php
+++ resources/views/components/events/footer.blade.php
@@ -2,8 +2,14 @@
     <footer class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             <div class="px-5 py-2">
-                <a href="https://phoenixvtc.com" class="text-base text-gray-500 hover:text-gray-400">
+                <a href="{{ route('events.home') }}" class="text-base text-gray-500 hover:text-gray-400">
                     Home
+                </a>
+            </div>
+
+            <div class="px-5 py-2">
+                <a href="https://phoenixvtc.com" class="text-base text-gray-500 hover:text-gray-400">
+                    Main Website
                 </a>
             </div>
 
