5270df148e9012d937a658fe9d42a86ee9feba9d
iDiego <me@diegor.nl>
2021-04-01 18:22:12 +0200

    Fix event card host on edit page [ch225] (#84)

--- app/Http/Livewire/Events/Management/ShowEdit.php
+++ app/Http/Livewire/Events/Management/ShowEdit.php
@@ -104,7 +104,8 @@ class ShowEdit extends Component
     {
         $this->event = $event;
 
-        $this->manage_event_users = Permission::findByName('manage events')->users;
+        $this->manage_event_users = Role::findByName('events')->users;
+        $this->manage_event_users = $this->manage_event_users->merge(Role::findByName('community interactions')->users);
         $this->manage_event_users = $this->manage_event_users->merge(Role::findByName('super admin')->users);
 
         $this->tmp_event_id = $this->event->tmp_event_id ?? null;
