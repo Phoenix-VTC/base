7962d488e968ffb3bf0d25a4f3f34d0daf255081
iDiego <me@diegor.nl>
2021-04-05 15:51:53 +0200

    Create navigation dropdown [ch212] (#93)
    * Move sidebar & navigation blades to "includes" directory [ch212]
    
    * Convert sidebar separator into component [ch212]
    
    * Convert sidebar group into component [ch212]
    
    * Convert sidebar items into component [ch212]
    
    * Create navigation dropdown [ch212]

---
+++ app/Http/Livewire/Components/Dropdown.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Request;
+use Livewire\Component;
+
+class Dropdown extends Component
+{
+    public string $title;
+    public string $activeRoute;
+    public string $icon;
+    public array $items;
+    public bool $showDropdown = false;
+
+    public function mount(): void
+    {
+        $this->showDropdown = Request::routeIs($this->activeRoute);
+    }
+
+    public function render()
+    {
+        return view('livewire.components.dropdown');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,9 +18,9 @@
 
 @section('body')
     <div class="h-screen flex overflow-hidden bg-gray-100" x-data="{ sidebarOpen: false }">
-        @include('components.sidebar')
+        @include('includes.sidebar')
         <div class="flex flex-col w-0 flex-1 overflow-hidden">
-            @include('components.navigation')
+            @include('includes.navigation')
 
             <main class="flex-1 relative overflow-y-auto focus:outline-none" tabindex="0">
                 <div class="py-6">
