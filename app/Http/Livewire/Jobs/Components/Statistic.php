0d348784f0af1286dfa2ac6381a07b98ef86e534
Diego <me@diegor.nl>
2021-04-09 13:39:50 +0200

    Create Jobs Statistic component [ch235]

---
+++ app/Http/Livewire/Jobs/Components/Statistic.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\Jobs\Components;
+
+use Livewire\Component;
+
+class Statistic extends Component
+{
+    public string $icon;
+    public string $title;
+    public string $content;
+    public string $changeNumber;
+    public bool $increased = true;
+
+    public function render()
+    {
+        return view('livewire.jobs.components.statistic');
+    }
+}
