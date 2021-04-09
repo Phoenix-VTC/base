d6eeeb56644910cd726f22974cef91c7976ac8e7
Diego <me@diegor.nl>
2021-04-09 13:58:53 +0200

    Create Job status Enum cast [ch235]

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\JobStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,6 +10,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Job extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'status' => JobStatus::class,
+    ];
 
     /**
      * Get the user that owns the job.
