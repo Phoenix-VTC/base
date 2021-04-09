c4d462dd791b9d0a7e02aceaece42fa4390ffa7e
Diego <me@diegor.nl>
2021-04-10 01:51:54 +0200

    Add empty state to job overview page [ch235]

--- resources/views/livewire/jobs/overview-page.blade.php
+++ resources/views/livewire/jobs/overview-page.blade.php
@@ -23,86 +23,95 @@
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                        <tr>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Status
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Game
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                From
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                To
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Distance
-                            </th>
-                            <th scope="col" class="relative px-6 py-3">
-                                <span class="sr-only">View</span>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($jobs as $job)
-                            <tr class="@if($loop->odd) bg-white @else bg-gray-50 @endif">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 capitalize">
-                                    @switch($job->status->key)
-                                        @case('Incomplete')
-                                        <div
-                                            class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-yellow-100 text-yellow-800">
-                                            <span>Incomplete</span>
-                                        </div>
-                                        @break
-                                        @case('PendingVerification')
-                                        <div
-                                            class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-purple-100 text-purple-800">
-                                            <span>Pending Verification</span>
-                                        </div>
-                                        @break
-                                        @case('Complete')
-                                        <div
-                                            class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-green-100 text-green-800">
-                                            <span>Completed</span>
-                                        </div>
-                                        @break
-                                        @default
-                                        <div
-                                            class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-gray-100 text-gray-800">
-                                            <span>Unknown Status</span>
-                                        </div>
-                                        @break
-                                    @endswitch
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ App\Models\Game::getAbbreviationById($job->game_id) }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ ucwords($job->pickupCity->real_name) ?? 'Unknown City' }}
-                                    ({{ ucwords($job->pickupCompany->name) ?? 'Unknown Company' }})
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ ucwords($job->destinationCity->real_name) ?? 'Unknown City' }}
-                                    ({{ ucwords($job->destinationCompany->name) ?? 'Unknown Company' }})
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $job->distance }} {{ App\Models\Game::getAbbreviationDistanceMetric($job->game_id) }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">View</a>
-                                </td>
+                    @empty(!$jobs->count())
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Status
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Game
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    From
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    To
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Distance
+                                </th>
+                                <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">View</span>
+                                </th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach($jobs as $job)
+                                <tr class="@if($loop->odd) bg-white @else bg-gray-50 @endif">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 capitalize">
+                                        @switch($job->status->key)
+                                            @case('Incomplete')
+                                            <div
+                                                class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-yellow-100 text-yellow-800">
+                                                <span>Incomplete</span>
+                                            </div>
+                                            @break
+                                            @case('PendingVerification')
+                                            <div
+                                                class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-purple-100 text-purple-800">
+                                                <span>Pending Verification</span>
+                                            </div>
+                                            @break
+                                            @case('Complete')
+                                            <div
+                                                class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-green-100 text-green-800">
+                                                <span>Completed</span>
+                                            </div>
+                                            @break
+                                            @default
+                                            <div
+                                                class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-gray-100 text-gray-800">
+                                                <span>Unknown Status</span>
+                                            </div>
+                                            @break
+                                        @endswitch
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ App\Models\Game::getAbbreviationById($job->game_id) }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ ucwords($job->pickupCity->real_name) ?? 'Unknown City' }}
+                                        ({{ ucwords($job->pickupCompany->name) ?? 'Unknown Company' }})
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ ucwords($job->destinationCity->real_name) ?? 'Unknown City' }}
+                                        ({{ ucwords($job->destinationCompany->name) ?? 'Unknown Company' }})
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        {{ $job->distance }} {{ App\Models\Game::getAbbreviationDistanceMetric($job->game_id) }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">View</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <x-empty-state :image="asset('img/illustrations/map_dark.svg')"
+                                       alt="Events illustration">
+                            Hmm, it looks like you don't have any submitted jobs yet.
+                            <br>
+                            Come back here when you've finished some!
+                        </x-empty-state>
+                    @endempty
                     {{ $jobs->links() }}
                 </div>
             </div>
