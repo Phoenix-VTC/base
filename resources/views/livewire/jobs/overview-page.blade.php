57b58bd2ff466aae955bfc92a70881ad89b50a49
iDiego <me@diegor.nl>
2021-04-19 18:58:40 +0200

    Create Job Create page [ch237] (#98)
    * Create searchable select component [ch237]
    
    * Add empty guarded array to Job model [ch237]
    
    * Create textarea input component [ch237]
    
    * Create job submit page [ch237]
    
    * Create JobObserver to submit new job payments [ch237]

--- resources/views/livewire/jobs/overview-page.blade.php
+++ resources/views/livewire/jobs/overview-page.blade.php
@@ -10,7 +10,7 @@
 
 @section('actions')
     <div class="ml-3">
-        <a href="#"
+        <a href="{{ route('jobs.submit') }}"
            class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <x-heroicon-s-truck class="-ml-1 mr-2 h-5 w-5"/>
             Submit New Job
@@ -19,6 +19,8 @@
 @endsection
 
 <div>
+    <x-alert/>
+
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
