fbd4b8888a218ddc6804552d04b4d90490891596
Diego <me@diegor.nl>
2021-04-22 13:21:57 +0200

    Redirect to job show page after submit [ch242]

--- resources/views/livewire/jobs/show-page.blade.php
+++ resources/views/livewire/jobs/show-page.blade.php
@@ -3,6 +3,8 @@
 @section('title', "Viewing Job #$job->id")
 
 <div>
+    <x-alert/>
+
     <div
         class="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
         <div class="space-y-6 lg:col-start-1 lg:col-span-2">
