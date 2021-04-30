06a05855a8376da13f81319cee14791442b89619
iDiego <me@diegor.nl>
2021-04-30 12:03:46 +0200

    Create permissions index page [ch282] (#123)

---
+++ app/Http/Livewire/UserManagement/Permissions/ShowIndexPage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire\UserManagement\Permissions;
+
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Component;
+use Spatie\Permission\Models\Permission;
+
+class ShowIndexPage extends Component
+{
+    public Collection $permissions;
+
+    public function mount(): void
+    {
+        session()->flash('alert', ['type' => 'info', 'title' => 'Heads-up!', 'message' => 'Permissions cannot be modified via the PhoenixBase.<br>Contact a Developer in order to do this.']);
+
+        $this->permissions = Permission::with('roles', 'users')->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.user-management.permissions.index-page')->extends('layouts.app');
+    }
+}
