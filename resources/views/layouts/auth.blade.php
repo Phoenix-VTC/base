2661d5600cf2daa2b511abde9c5bb4738aee63e5
iDiego <me@diegor.nl>
2021-05-04 17:26:11 +0200

    Renew auth pages [ch289] (#129)
    * Implement new auth layout [ch289]
    
    * Implement new auth layout on login page [ch289]
    
    * Remove old/unused registration code [ch289]
    
    * Implement new auth layout on password reset email page [ch289]
    
    * Implement new auth layout on password reset page [ch289]
    
    * Implement new auth layout on welcome page [ch289]
    
    * Implement new auth layout on confirm password page [ch289]
    
    * Fix back to login href
    
    Co-authored-by: iDiegoNL <$JkPBploP^BZ9m4grlDK8>

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.base')
 
 @section('body')
-    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
-        @yield('content')
+    <div class="min-h-screen bg-white flex">
+        <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div class="mx-auto w-full max-w-sm lg:w-96">
+                @yield('content')
 
-        @isset($slot)
-            {{ $slot }}
-        @endisset
+                @isset($slot)
+                    {{ $slot }}
+                @endisset
+            </div>
+        </div>
+        <div class="hidden lg:block relative w-0 flex-1">
+            <img class="absolute inset-0 h-full w-full object-cover"
+                 src="https://phoenix-base.s3.nl-ams.scw.cloud/images/227300_20210216162827_1.png"
+                 alt="A Phoenix truck and trailer with hot-air balloons in the background">
+        </div>
     </div>
 @endsection
