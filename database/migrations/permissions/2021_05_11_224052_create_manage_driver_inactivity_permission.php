3f7369e609e7fb3f64da18c24ca18153e9de7460
Diego <me@diegor.nl>
2021-05-12 00:50:42 +0200

    Create manage driver inactivity permission [ch314]

---
+++ database/migrations/permissions/2021_05_11_224052_create_manage_driver_inactivity_permission.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateManageDriverInactivityPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $permission = Permission::create(['name' => 'manage driver inactivity']);
+
+        $management = Role::findByName('management');
+        $management->givePermissionTo($permission);
+
+        $human_resources = Role::findByName('human resources');
+        $human_resources->givePermissionTo($permission);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Permission::where('name', 'manage driver inactivity')
+            ->firstOrFail()
+            ->delete();
+    }
+}
