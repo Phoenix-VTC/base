3716d78a2f45eba92091a9ccef3f0e846af329ea
Diego <me@diegor.nl>
2021-05-17 21:28:08 +0200

    Add link to user profile in Recent Jobs profile picture [ch318]

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -308,13 +308,14 @@
                                                     <span class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
                                                           aria-hidden="true"></span>
                                                 @endif
-                                                <div class="relative flex space-x-3"><div>
-                                            <span
-                                                class="h-8 w-8 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white">
-                                                <img class="w-8 h-8 rounded-full"
-                                                     src="{{ $job->user->profile_picture ?? '' }}"
-                                                     alt="{{ $job->user->username ?? 'Unknown Username' }}"/>
-                                            </span>
+                                                <div class="relative flex space-x-3">
+                                                    <div>
+                                                        <a class="h-8 w-8 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white"
+                                                           href="{{ route('users.profile', $user) }}">
+                                                            <img class="w-8 h-8 rounded-full"
+                                                                 src="{{ $job->user->profile_picture ?? '' }}"
+                                                                 alt="{{ $job->user->username ?? 'Unknown Username' }}"/>
+                                                        </a>
                                                     </div>
                                                     <div class="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
                                                         <div>
