19c189355c46cec02210a9a1b6f5bd0be3eb00ba
Diego <me@diegor.nl>
2021-05-18 19:53:41 +0200

    Create GameDataController for Select2 Ajax [ch258]

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\Select2\GameDataController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// ->middleware(['auth', 'can:submit jobs'])
+Route::prefix('game-data/{game}')->name('game-data.')->group(function ($group) {
+    Route::get('cities', [GameDataController::class, 'indexCities'])->name('cities');
+
+    Route::get('companies', [GameDataController::class, 'indexCompanies'])->name('companies');
+
+    Route::get('cargos', [GameDataController::class, 'indexCargos'])->name('cargos');
+
+    // Validate game param for each route
+    foreach ($group->getRoutes() as $route) {
+        $route->whereNumber('game');
+    }
+});
