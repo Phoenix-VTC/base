d746fc8519ff5e38a8cbd69a8ebcbfe48eea4a0f
Diego <me@diegor.nl>
2021-06-09 20:51:14 +0200

    Create inactive drivers page [ch314]

--- app/Http/Livewire/UserManagement/DriverInactivity/ShowIndexPage.php
+++ app/Http/Livewire/UserManagement/DriverInactivity/ShowIndexPage.php
@@ -4,43 +4,55 @@ namespace App\Http\Livewire\UserManagement\DriverInactivity;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class ShowIndexPage extends Component
 {
-    private LengthAwarePaginator $inactive_drivers;
+    // Request data
+    public string $month;
+    public string $orderBy;
 
-    public function mount(): void
+    public function mount(Request $request): void
     {
-        $this->inactive_drivers = $this->getInactiveDrivers();
+        session()->now('alert', ['type' => 'info', 'title' => 'Heads-up!', 'message' => 'Make sure to check the inactivity on <a href="https://trucksbook.eu/drivers" target="_blank" class="font-bold">TrucksBook</a> as well.']);
+
+        $month = $request->get('month');
+        $orderBy = $request->get('orderBy');
+
+        // Set month value to current month if request isn't a month number
+        if ($month >= 1 && $month <= 12) {
+            $this->month = $month;
+        } else {
+            $this->month = date('m');
+        }
+
+        // Set leaderboardMeasurement value to distance if request isn't 'income', 'deliveries' or 'distance'
+        if (in_array($orderBy, ['income', 'deliveries', 'distance'])) {
+            $this->orderBy = $orderBy;
+        } else {
+            $this->orderBy = 'distance';
+        }
     }
 
     public function render()
     {
-        return view('livewire.user-management.driver-inactivity.index-page', ['inactive_drivers' => $this->inactive_drivers])->extends('layouts.app');
+        $inactive_drivers = User::withCount(['jobs' => function (Builder $q) {
+            $q->whereMonth('finished_at', $this->month);
+        }])->withSum(['jobs:distance' => function (Builder $q) {
+            $q->whereMonth('finished_at', $this->month);
+        }])->with(['roles'])
+            ->with([
+                "vacation_requests" => function ($q) {
+                    $q->whereMonth('start_date', '<=', $this->month)
+                        ->whereMonth('end_date', '>=', $this->month);
+                },
+            ])
+            ->orderBy('jobs_distance_sum')
+            ->paginate(30);
 
-//        return view('livewire.user-management.driver-inactivity.index-page', [
-//            'inactive_drivers' => User::whereHas('jobs', function (Builder $query) {
-//                $query->whereMonth('finished_at', date('m'))
-//                    ->groupBy('id')
-//                    ->havingRaw('SUM(distance) < ?', [1000]);
-//            })
-//                ->withCount(['jobs AS total_distance' => function (Builder $query) {
-//                    $query->whereMonth('finished_at', date('m'));
-//                }])
-//                ->paginate(30)
-//        ])->extends('layouts.app');
-    }
-
-    public function getInactiveDrivers(): LengthAwarePaginator
-    {
-        $no_jobs = User::whereDoesntHave('jobs', function (Builder $query) {
-            $query->whereMonth('finished_at', date('m'));
-        })->withCount(['jobs AS total_jobs', 'jobs AS total_distance' => function (Builder $query) {
-            $query->whereMonth('finished_at', date('m'));
-        }])->paginate(30);
-
-        return $no_jobs;
+        return view('livewire.user-management.driver-inactivity.index-page', [
+            'inactive_drivers' => $inactive_drivers
+        ])->extends('layouts.app');
     }
 }
--- resources/views/livewire/user-management/driver-inactivity/index-page.blade.php
+++ resources/views/livewire/user-management/driver-inactivity/index-page.blade.php
@@ -1,6 +1,38 @@
 {{-- The best athlete wants his opponent at his best. --}}
 
-@section('title', 'Driver Inactivity')
+@section('title', 'Driver Inactivity (' . Carbon\Carbon::create()->month($month)->startOfMonth()->isoFormat('MMMM') . ')')
+
+@section('actions')
+    @if($month !== date('m'))
+        <div class="ml-3">
+            <a href="{{ route('user-management.driver-inactivity.index') }}"
+               class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <x-heroicon-s-calendar class="-ml-1 mr-2 h-5 w-5 text-gray-500"/>
+                Back to {{ date('M') }}
+            </a>
+        </div>
+    @endif
+
+    <div class="ml-3">
+        <span class="relative z-0 inline-flex shadow-sm rounded-md">
+            <a href="{{ route('user-management.driver-inactivity.index', ['month' => Carbon\Carbon::create()->month($month)->startOfMonth()->subMonth()->format('m'), 'orderBy' => $orderBy]) }}"
+               class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                <span class="sr-only">Previous Month</span>
+                <x-heroicon-s-chevron-left class="h-5 w-5"/>
+            </a>
+            <span
+                class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
+                {{ Carbon\Carbon::create()->month($month)->startOfMonth()->format('M') }}
+            </span>
+            <a href="{{ route('user-management.driver-inactivity.index', ['month' => Carbon\Carbon::create()->month($month)->startOfMonth()->addMonth()->format('m'), 'orderBy' => $orderBy]) }}"
+               disabled
+               class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                <span class="sr-only">Next Month</span>
+                <x-heroicon-s-chevron-right class="h-5 w-5"/>
+            </a>
+        </span>
+    </div>
+@endsection
 
 <div>
     <x-alert/>
@@ -15,7 +47,7 @@
                             <tr>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    User ID
+                                    ID
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -35,7 +67,7 @@
                                 </th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    LOA
+                                    On LOA
                                 </th>
                                 <th scope="col" class="relative px-6 py-3">
                                     <span class="sr-only">View Job Overview</span>
@@ -44,7 +76,7 @@
                             </thead>
                             <tbody>
                             @foreach($inactive_drivers as $user)
-                                <tr class="@if($loop->odd) bg-white @else bg-gray-50 @endif">
+                                <tr class="@if($user->jobs_distance_sum < 1000) bg-red-100 @else bg-green-100 @endif">
                                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 capitalize">
                                         {{ $user->id }}
                                     </td>
@@ -53,19 +85,25 @@
                                            class="text-indigo-600 hover:text-indigo-900">
                                             {{ $user->username ?? 'Deleted User' }}
                                         </a>
+                                        @if($user->hasRole('phoenix staff'))
+                                            <span
+                                                class="inline-flex items-center ml-1 px-2 py-0.5 rounded text-xs font-medium bg-yellow-100 text-yellow-800">
+                                                Staff
+                                            </span>
+                                        @endif
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $user->total_jobs }}
+                                        {{ $user->jobs_count ?? 0 }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $user->total_distance }}
+                                        {{ $user->jobs_distance_sum ?? 0 }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                         {{ $user->created_at->toFormattedDateString() }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        @if($user->active_vacation_requests->count())
-                                            @foreach($user->active_vacation_requests as $vacation_request)
+                                        @if($user->vacation_requests->count())
+                                            @foreach($user->vacation_requests as $vacation_request)
                                                 <span
                                                     class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800 mb-2">
                                                     From {{ $vacation_request->start_date->toFormattedDateString() }}
@@ -92,14 +130,14 @@
                             </tbody>
                         </table>
                     @else
-                        <x-empty-state :image="asset('img/illustrations/map_dark.svg')"
-                                       alt="Map illustration">
-                            Hmm, it looks like you don't have any submitted jobs yet.
+                        <x-empty-state :image="asset('img/illustrations/no_data.svg')"
+                                       alt="No Data illustration">
+                            Hmm, it looks like there isn't any Driver Inactivity data yet.
                             <br>
-                            Come back here when you've finished some!
+                            Why don't you check back later?
                         </x-empty-state>
                     @endempty
-                    {{ $inactive_drivers->links() }}
+                    {{ $inactive_drivers->appends(request()->all())->links() }}
                 </div>
             </div>
         </div>
