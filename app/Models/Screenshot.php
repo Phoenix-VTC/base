2c88de2e516ff84d5b9fe695dd08e9f232ed2d79
Diego <me@diegor.nl>
2021-06-12 02:43:33 +0200

    Create Screenshot model and migration [ch340]

---
+++ app/Models/Screenshot.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Screenshot extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the user that owns the screenshot.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
