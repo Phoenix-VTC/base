363322735ea54c4c64ab84d0439f4cd0d8c53b27
Diego <me@diegor.nl>
2021-06-14 23:24:10 +0200

    Install VentureCraft/revisionable [ch341]

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
+        Venturecraft\Revisionable\RevisionableServiceProvider::class,
 
     ],
 
