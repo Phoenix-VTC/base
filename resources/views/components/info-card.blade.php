9de31ce03db618e813034d959fdd8efb3ff4e93a
Diego <me@diegor.nl>
2021-06-14 23:23:50 +0200

    Make some modifications to the info-card component [ch341]

--- resources/views/components/info-card.blade.php
+++ resources/views/components/info-card.blade.php
@@ -1,15 +1,28 @@
 <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
 
+@props([
+    'title' => null,
+    'description' => null,
+    'footer' => null
+])
+
 <div class="bg-white shadow overflow-hidden sm:rounded-lg">
     <div class="px-4 py-5 sm:px-6">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
             {{ $title }}
         </h3>
-        <p class="mt-1 max-w-2xl text-sm text-gray-500">
-            {{ $description }}
-        </p>
+        @if($description)
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                {{ $description }}
+            </p>
+        @endif
     </div>
     <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
         {{ $slot }}
     </div>
+    @if($footer)
+        <div class="bg-gray-50 px-4 py-6 sm:px-6">
+            {{ $footer }}
+        </div>
+    @endif
 </div>
