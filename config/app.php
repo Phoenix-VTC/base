5af4eaea547b171f8e98e245480f996c3a07b6d5
iDiego <me@diegor.nl>
2021-06-16 01:17:38 +0200

    Create User Impersonation [ch317] (#162)
    * Create impersonate users permission [ch317]
    
    * Give developers permission to manage users [ch317]
    
    * Install and configure lab404/laravel-impersonate [ch317]
    
    * Setup impersonation authorization [ch317]
    
    * Add developer role to the sidebar management separator hasanyrole [ch317]
    
    * Implement User Impersonation [ch317]

--- config/app.php
+++ config/app.php
@@ -172,6 +172,7 @@ return [
         App\Providers\HorizonServiceProvider::class,
         NotificationChannels\Discord\DiscordServiceProvider::class,
         \SocialiteProviders\Manager\ServiceProvider::class,
+        Lab404\Impersonate\ImpersonateServiceProvider::class,
 
         /*
          * Application Service Providers...
