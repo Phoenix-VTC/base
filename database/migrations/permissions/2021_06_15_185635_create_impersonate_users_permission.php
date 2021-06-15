95ca8323bee68cd915ef51080e361ed19ec90b05
Diego <me@diegor.nl>
2021-06-15 20:57:42 +0200

    Create impersonate users permission [ch317]

---
+++ database/migrations/permissions/2021_06_15_185635_create_impersonate_users_permission.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateImpersonateUsersPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $permission = Permission::create(['name' => 'impersonate users']);
+
+        $developer = Role::findByName('developer');
+        $developer->givePermissionTo($permission);
+
+        $management = Role::findByName('management');
+        $management->givePermissionTo($permission);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Permission::where('name', 'impersonate users')
+            ->firstOrFail()
+            ->delete();
+    }
+}
