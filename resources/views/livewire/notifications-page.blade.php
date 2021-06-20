a32095ab0b567999a9db21a6d4b6f8c470eb7045
iDiego <me@diegor.nl>
2021-06-20 20:30:10 +0200

    Add mark all as read button to notifications page [ch343] (#175)
    * Add null coalescing operator to notifications [ch343]
    
    * Add mark all as read button to notifications page [ch343]

--- resources/views/livewire/notifications-page.blade.php
+++ resources/views/livewire/notifications-page.blade.php
@@ -2,6 +2,16 @@
 
 @section('title', 'My notifications')
 
+@section('actions')
+    <div class="ml-3">
+        <a href="{{ route('notifications.markAllAsRead') }}"
+           class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <x-heroicon-s-check class="-ml-1 mr-2 h-5 w-5 text-gray-500"/>
+            Mark all as read
+        </a>
+    </div>
+@endsection
+
 <div class="space-y-5">
     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
         <div class="px-4 py-5 sm:px-6">
@@ -20,7 +30,7 @@
                                 <button type="submit"
                                         class="text-sm font-semibold text-gray-800 hover:underline focus:outline-none">
                                     <span class="absolute inset-0" aria-hidden="true"></span>
-                                    {{ Str::limit($notification->data['title'], 20) }}
+                                    {{ Str::limit($notification->data['title'] ?? '', 20) }}
                                 </button>
                             </div>
                             <time datetime="2021-01-27T16:35"
@@ -30,7 +40,7 @@
                         </form>
                         <div class="mt-1">
                             <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                {{ $notification->data['content'] }}
+                                {{ $notification->data['content'] ?? '' }}
                             </p>
                         </div>
                     </li>
@@ -62,7 +72,7 @@
                                 <button type="submit"
                                         class="text-sm font-semibold text-gray-800 hover:underline focus:outline-none">
                                     <span class="absolute inset-0" aria-hidden="true"></span>
-                                    {{ Str::limit($notification->data['title'], 20) }}
+                                    {{ Str::limit($notification->data['title'] ?? '', 20) }}
                                 </button>
                             </div>
                             <time datetime="2021-01-27T16:35"
@@ -72,7 +82,7 @@
                         </form>
                         <div class="mt-1">
                             <p class="mt-1 text-sm text-gray-600 line-clamp-2">
-                                {{ $notification->data['content'] }}
+                                {{ $notification->data['content'] ?? '' }}
                             </p>
                         </div>
                     </li>
