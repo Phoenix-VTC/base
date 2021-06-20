e658e0030c8724bdbf27e5db1dc413aabb72ff67
Diego <me@diegor.nl>
2021-06-20 02:04:08 +0200

    Up navbar notification title length limit [ch337]

--- resources/views/includes/navigation.blade.php
+++ resources/views/includes/navigation.blade.php
@@ -24,7 +24,7 @@
                                     <button type="submit"
                                             class="text-sm font-semibold text-gray-800 hover:underline focus:outline-none">
                                         <span class="absolute inset-0" aria-hidden="true"></span>
-                                        {{ Str::limit($notification->data['title'], 20) }}
+                                        {{ Str::limit($notification->data['title'], 30) }}
                                     </button>
                                 </div>
                                 <time datetime="2021-01-27T16:35"
