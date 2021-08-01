fe69cef35764bd28fb7791b03173dd3c1f6c7518
Diego <me@diegor.nl>
2021-08-01 19:53:30 +0200

    Fix screenshot image rounding [ch378]

--- resources/views/livewire/screenshot-hub/show-page.blade.php
+++ resources/views/livewire/screenshot-hub/show-page.blade.php
@@ -31,9 +31,9 @@
     <div
         class="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
         <div class="space-y-6 lg:col-start-1 lg:col-span-2">
-            <div class="block w-full h-full rounded-lg overflow-hidden">
+            <div class="block w-full h-full overflow-hidden">
                 <a href="{{ $screenshot->image_url }}" target="_blank">
-                    <img src="{{ $screenshot->image_url }}" alt="{{ $screenshot->title }}" class="object-cover">
+                    <img src="{{ $screenshot->image_url }}" alt="{{ $screenshot->title }}" class="object-cover rounded-lg">
                 </a>
             </div>
         </div>
