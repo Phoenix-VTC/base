ccf5a1db864d52ff2ec4bcd9c7ec8e60128558d3
iDiego <me@diegor.nl>
2021-09-08 13:24:04 +0200

    Hide "Reward Event XP" button if event hasn't finished yet [ch426] (#212)

--- resources/views/livewire/events/management/attendee-management.blade.php
+++ resources/views/livewire/events/management/attendee-management.blade.php
@@ -27,17 +27,19 @@
         </a>
     </div>
 
-    <form action="{{ route('event-management.reward-event-xp', $event) }}" method="POST">
-        @csrf
+    @if (!$event->completed && $event->is_past)
+        <form action="{{ route('event-management.reward-event-xp', $event) }}" method="POST">
+            @csrf
 
-        <div class="ml-3">
-            <button type="submit"
-                    class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <x-heroicon-s-check class="-ml-1 mr-2 h-5 w-5"/>
-                Reward Event XP
-            </button>
-        </div>
-    </form>
+            <div class="ml-3">
+                <button type="submit"
+                        class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <x-heroicon-s-check class="-ml-1 mr-2 h-5 w-5"/>
+                    Reward Event XP
+                </button>
+            </div>
+        </form>
+    @endif
 @endsection
 
 <div>
@@ -58,7 +60,7 @@
                                 <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                                     <div>
                                         <p class="text-sm font-medium text-black truncate">{{ $attendee->user->username ?? 'Deleted User' }}</p>
-                                        <div class="mt-2 flex items-center text-sm text-gray-500">
+                                        <div class="mt-2 fle x items-center text-sm text-gray-500">
                                             <div class="sm:flex">
                                                 <p class="flex items-center text-sm text-gray-500">
                                                     @if($attendee->attending->value === App\Enums\Attending::Yes)
