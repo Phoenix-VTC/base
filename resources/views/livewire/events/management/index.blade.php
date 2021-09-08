b0ef5c4de42a380b696d6b8b67ef68029b088b93
iDiego <me@diegor.nl>
2021-09-08 21:38:42 +0200

    Change Event Start Date and Time to Departure [sc-271] (#215)

--- resources/views/livewire/events/management/index.blade.php
+++ resources/views/livewire/events/management/index.blade.php
@@ -34,7 +34,7 @@
                                     </th>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Start Date
+                                        Departure Date
                                     </th>
                                     <th scope="col" class="relative px-6 py-3">
                                         <span class="sr-only">Edit</span>
