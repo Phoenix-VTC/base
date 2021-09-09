69584cd16341c1bd65d8bc23b1ddf809b655e98d
Diego <me@diegor.nl>
2021-09-09 15:21:19 +0200

    Include TMP URL in acceptance email [sc-407]

--- resources/views/emails/driver-application/application-accepted.blade.php
+++ resources/views/emails/driver-application/application-accepted.blade.php
@@ -5,6 +5,8 @@
 
 We are very happy to share with you that have been accepted into PhoenixVTC!
 
+As a next step, please also **[apply to our TruckersMP page](https://truckersmp.com/vtc/30294)**. The application text can be something simple like "I already am a member of Phoenix". This is just to finalize your application process.
+
 To activate your {{ config('app.name') }} account, please press the button below.
 
 @component('mail::button', ['url' => route('welcome', $user->welcome_token)])
