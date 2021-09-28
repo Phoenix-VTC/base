0999a3efb5e770323d4ab82470e24d6143242712
Diego <me@diegor.nl>
2021-09-28 11:44:24 +0200

    Create blocklist permissions [sc-439]

---
+++ database/migrations/permissions/2021_09_27_144828_create_blocklist_permissions.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateBlocklistPermissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        $viewBlocklist = Permission::create(['name' => 'view blocklist']);
+        $createBlocklist = Permission::create(['name' => 'create blocklist']);
+        $deleteBlocklist = Permission::create(['name' => 'delete blocklist']);
+
+        $management = Role::findByName('management');
+        $management->givePermissionTo([
+            $viewBlocklist,
+            $createBlocklist,
+            $deleteBlocklist
+        ]);
+
+        $recruitment = Role::findByName('recruitment');
+        $recruitment->givePermissionTo([
+            $viewBlocklist,
+            $createBlocklist
+        ]);
+
+        $humanResources = Role::findByName('human resources');
+        $humanResources->givePermissionTo([
+            $viewBlocklist,
+            $createBlocklist
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Permission::query()
+            ->where('name', 'view blocklist')
+            ->firstOrFail()
+            ->delete();
+
+        Permission::query()
+            ->where('name', 'create blocklist')
+            ->firstOrFail()
+            ->delete();
+
+        Permission::query()
+            ->where('name', 'delete blocklist')
+            ->firstOrFail()
+            ->delete();
+    }
+}
