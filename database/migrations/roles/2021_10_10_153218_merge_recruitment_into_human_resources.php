79eb89bbe143d8af734d51ba628180ac76857695
iDiego <me@diegor.nl>
2021-10-10 17:55:53 +0200

    Implement staff changes [sc-449] (#231)
    * Create RemoveCommunityInteractionsRole migration [sc-449]
    
    * Create MergeRecruitmentIntoHumanResources migration [sc-449]
    
    * Rename all recruitment-related stuff to human resources [sc-449]

---
+++ database/migrations/roles/2021_10_10_153218_merge_recruitment_into_human_resources.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Role;
+
+class MergeRecruitmentIntoHumanResources extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Role::where('name', 'recruitment')
+            ->firstOrFail()
+            ->delete();
+
+        $humanResources = Role::findByName('human resources');
+        $humanResources->givePermissionTo('handle driver applications');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Role::create(['name' => 'recruitment']);
+
+        $humanResources = Role::findByName('human resources');
+        $humanResources->revokePermissionTo('handle driver applications');
+    }
+}
