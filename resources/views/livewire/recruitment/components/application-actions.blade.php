3a154912c3daef075e83304161935c22476475a4
Diego <me@diegor.nl>
2021-10-15 14:17:07 +0200

    Change application-actions a tags to button tags [sc-26]

--- resources/views/livewire/recruitment/components/application-actions.blade.php
+++ resources/views/livewire/recruitment/components/application-actions.blade.php
@@ -121,8 +121,8 @@
                          class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100"
                          role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                         <div class="py-1">
-                            <a href="#" wire:click="setStatus('pending')"
-                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            <button wire:click="setStatus('pending')"
+                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full"
                                role="menuitem">
                                 {{-- clock --}}
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
@@ -133,9 +133,9 @@
                                           d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                 </svg>
                                 Pending
-                            </a>
-                            <a href="#" wire:click="setStatus('incomplete')"
-                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            </button>
+                            <button wire:click="setStatus('incomplete')"
+                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full"
                                role="menuitem">
                                 {{-- document --}}
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
@@ -146,9 +146,9 @@
                                           d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"/>
                                 </svg>
                                 Incomplete
-                            </a>
-                            <a href="#" wire:click="setStatus('awaiting_response')"
-                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            </button>
+                            <button wire:click="setStatus('awaiting_response')"
+                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full"
                                role="menuitem">
                                 {{-- chat --}}
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
@@ -159,9 +159,9 @@
                                           d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
                                 </svg>
                                 Awaiting Response
-                            </a>
-                            <a href="#" wire:click="setStatus('investigation')"
-                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            </button>
+                            <button wire:click="setStatus('investigation')"
+                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full"
                                role="menuitem">
                                 {{-- document-search --}}
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
@@ -172,11 +172,11 @@
                                           d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"/>
                                 </svg>
                                 Investigation
-                            </a>
+                            </button>
                         </div>
                         <div class="py-1">
-                            <a href="#" wire:click="deny"
-                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            <button wire:click="deny"
+                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full"
                                role="menuitem">
                                 {{-- ban --}}
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
@@ -187,11 +187,11 @@
                                           d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"/>
                                 </svg>
                                 Deny
-                            </a>
+                            </button>
                         </div>
                         {{-- <div class="py-1">
-                            <a href="#" wire:click="blacklist"
-                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                            <button wire:click="blacklist"
+                               class="group flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full"
                                role="menuitem">
                                 shield-exclamation
                                 <svg class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
@@ -201,7 +201,7 @@
                                           d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01"/>
                                 </svg>
                                 Blacklist
-                            </a>
+                            </button>
                         </div> --}}
                     </div>
                 </div>
