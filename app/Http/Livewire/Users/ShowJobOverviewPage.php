185c5600d6de336731d924c221081d260a01ee9b
Andrew Haskell <pilotguy50@gmail.com>
2021-10-27 05:42:54 -0400

    Merge Personal Job Overview & User Job Overview pages [sc-325] (#253)
    * Merge Personal Job Overview User Job Overview pages [sc-325]
    
    * Redirect personal job overview page to user job overview page [sc-325]
    
    * Change personal overview link in navbar to user job overview page & Allow navbar dropdowns to pass parameters to the url [sc-325]
    
    * Fix job overview page always returning authenticated user's balance [sc-325]

--- app/Http/Livewire/Users/ShowJobOverviewPage.php
+++ app/Http/Livewire/Users/ShowJobOverviewPage.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Livewire\Users;
 
+use App\Enums\JobStatus;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ShowJobOverviewPage extends Component
@@ -14,6 +16,9 @@ class ShowJobOverviewPage extends Component
         return view('livewire.users.job-overview-page', [
             'jobs' => $this->user
                 ->jobs()
+                ->when(Auth::id() != $this->user->id, function ($query) {
+                    return $query->where('status', JobStatus::Complete);
+                })
                 ->with([
                     'pickupCity',
                     'destinationCity',
