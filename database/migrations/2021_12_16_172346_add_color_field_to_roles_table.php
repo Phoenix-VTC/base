e09d16c89d75efcb62c5901f50afa34564ecae72
Andrew Haskell <pilotguy50@gmail.com>
2021-12-16 18:07:59 -0500

    Refactor user role colors [sc-520]

---
+++ database/migrations/2021_12_16_172346_add_color_field_to_roles_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColorFieldToRolesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->string('badge_color')->default('#f4f5f7')->after('name');
+            $table->string('text_color')->default('#1f2937')->after('badge_color');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->dropColumn('color');
+            $table->dropColumn('text_color');
+        });
+    }
+}
