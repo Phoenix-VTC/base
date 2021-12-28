dd5e75501dc37354f1555cea82c732df086d5316
Diego <me@diegor.nl>
2021-12-28 20:43:28 +0100

    Implement Filament Forms on company index/edit page [sc-440]

--- resources/views/livewire/game-data/companies.blade.php
+++ resources/views/livewire/game-data/companies.blade.php
@@ -11,53 +11,19 @@
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit.prevent="submit">
-            <div class="shadow overflow-hidden sm:rounded-md">
-                <div class="px-4 py-5 bg-white sm:p-6">
-                    <div class="grid grid-cols-6 gap-6">
-                        @csrf
-
-                        <x-input.group label="Name" for="name" :error="$errors->first('name')" col-span="3">
-                            <x-input.text wire:model.lazy="name" type="text" id="name"
-                                          :error="$errors->first('name')" autocomplete="off"/>
-                        </x-input.group>
-
-                        <br>
-
-                        <x-input.group label="Category" for="category" :error="$errors->first('category')" col-span="3">
-                            <x-input.text wire:model.lazy="category" type="text" id="category"
-                                          :error="$errors->first('category')"/>
-                        </x-input.group>
-
-                        <x-input.group label="Specialization" for="specialization" :error="$errors->first('specialization')" col-span="3">
-                            <x-input.text wire:model.lazy="specialization" type="text" id="specialization"
-                                          :error="$errors->first('specialization')"/>
-                        </x-input.group>
-
-                        <x-input.group label="DLC" for="dlc" :error="$errors->first('dlc')" col-span="3">
-                            <x-input.text wire:model.lazy="dlc" type="text" id="dlc"
-                                          :error="$errors->first('dlc')"/>
-                        </x-input.group>
-
-                        <x-input.group label="Mod" for="mod" :error="$errors->first('mod')" col-span="3">
-                            <x-input.text wire:model.lazy="mod" type="text" id="mod"
-                                          :error="$errors->first('mod')"/>
-                        </x-input.group>
-
-                        <x-input.group label="Game" for="game_id" :error="$errors->first('game_id')">
-                            <x-input.select wire:model.lazy="game_id" id="game_id">
-                                <option value="1">Euro Truck Simulator 2</option>
-                                <option value="2">American Truck Simulator</option>
-                            </x-input.select>
-                        </x-input.group>
-                    </div>
+            <x-app-ui::card>
+                <div class="mt-3">
+                    {{ $this->form }}
                 </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button type="submit"
-                            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-600 text-base font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:ml-3 sm:w-auto sm:text-sm">
-                        Add
-                    </button>
-                </div>
-            </div>
+
+                <x-slot name="footer">
+                    <x-app-ui::card.actions>
+                        <x-app-ui::button type="submit">
+                            Add
+                        </x-app-ui::button>
+                    </x-app-ui::card.actions>
+                </x-slot>
+            </x-app-ui::card>
         </form>
     </div>
 </div>
