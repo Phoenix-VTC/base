5b05f3cfaf0225d9b490df2df7ac88ef43cdf1db
Diego <me@diegor.nl>
2021-12-28 20:34:24 +0100

    Implement Filament Forms on city index/edit page [sc-440]

--- app/Http/Livewire/GameData/Cities/ShowIndexPage.php
+++ app/Http/Livewire/GameData/Cities/ShowIndexPage.php
@@ -3,54 +3,92 @@
 namespace App\Http\Livewire\GameData\Cities;
 
 use App\Models\City;
-use Illuminate\Validation\Rule;
-use Illuminate\View\View;
+use Filament\Forms;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Livewire\Component;
 
-class ShowIndexPage extends Component
+class ShowIndexPage extends Component implements HasForms
 {
-    public string $real_name = '';
-    public string $name = '';
-    public string $country = '';
-    public string $dlc = '';
-    public string $mod = '';
-    public string $game_id = '1';
-    public string $x = '';
-    public string $z = '';
+    use InteractsWithForms;
 
-    public function rules(): array
+    public $real_name;
+    public $name;
+    public $country;
+    public $dlc;
+    public $mod;
+    public $game_id = 1;
+    public $x;
+    public $z;
+
+    public function render()
     {
-        return [
-            'real_name' => ['required'],
-            'name' => ['required'],
-            'country' => ['required'],
-            'dlc' => ['sometimes'],
-            'mod' => ['sometimes'],
-            'game_id' => ['required', 'integer', Rule::in(['1', '2'])],
-            'x' => ['sometimes', 'integer'],
-            'z' => ['sometimes', 'integer'],
-        ];
+        return view('livewire.game-data.cities')->extends('layouts.app');
     }
 
-    public function render(): View
+    protected function getFormSchema(): array
     {
-        return view('livewire.game-data.cities')
-            ->extends('layouts.app');
+        return [
+            Forms\Components\Grid::make()
+                ->schema([
+                    Forms\Components\TextInput::make('real_name')
+                        ->helperText('Example: Frankfurt am Main')
+                        ->required(),
+
+                    Forms\Components\TextInput::make('name')
+                        ->helperText('Example: frankfurt_am_main')
+                        ->required(),
+
+                    Forms\Components\Grid::make()
+                        ->columns(1)
+                        ->schema([
+                            Forms\Components\TextInput::make('country')
+                                ->label('Country or US state')
+                                ->required(),
+                        ]),
+
+                    Forms\Components\TextInput::make('dlc')
+                        ->label('DLC'),
+
+                    Forms\Components\TextInput::make('mod'),
+
+                    Forms\Components\Grid::make()
+                        ->schema([
+                            Forms\Components\Select::make('game_id')
+                                ->label('Game')
+                                ->options([
+                                    1 => 'Euro Truck Simulator 2',
+                                    2 => 'American Truck Simulator',
+                                ])
+                                ->required()
+                        ]),
+
+                    Forms\Components\TextInput::make('x')
+                        ->numeric()
+                        ->label('X-coordinate')
+                        ->helperText('Optional, but please try to specify.'),
+
+                    Forms\Components\TextInput::make('z')
+                        ->numeric()
+                        ->label('Z-coordinate')
+                        ->helperText('Optional, but please try to specify.'),
+                ]),
+        ];
     }
 
     public function submit(): void
     {
-        $this->validate();
+        $validatedData = $this->form->getState();
 
         $city = City::create([
-            'real_name' => $this->real_name,
-            'name' => $this->name,
-            'country' => $this->country,
-            'dlc' => $this->dlc,
-            'mod' => $this->mod,
-            'game_id' => (int)$this->game_id,
-            'x' => $this->x ?: null,
-            'z' => $this->z ?: null,
+            'real_name' => $validatedData['real_name'],
+            'name' => $validatedData['name'],
+            'country' => $validatedData['country'],
+            'dlc' => $validatedData['dlc'],
+            'mod' => $validatedData['mod'],
+            'game_id' => $validatedData['game_id'],
+            'x' => $validatedData['x'] ?: null,
+            'z' => $validatedData['z'] ?: null,
         ]);
 
         $this->reset();
