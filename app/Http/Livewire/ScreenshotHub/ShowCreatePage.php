7f317492e0cf5445a69219aefb44407f0683d0f9
Diego <me@diegor.nl>
2021-12-29 16:40:50 +0100

    Implement and switch to Filament Forms v2 [sc-440] (#291)
    * Fix responsiveness on job create page [sc-440]
    
    * Improve dropdown search code [sc-440]
    
    * Implement Filament Forms on job edit page [sc-440]
    
    * Make vacation request start_date nullable [sc-440]
    
    * Update NewVacationRequest notification [sc-440]
    
    So that the `starts in` embed field only shows if the user isn't leaving
    
    * Implement Filament Forms on vacation request create page [sc-440]
    
    * Add currency symbols to income inputs [sc-440]
    
    * Disable estimated_income editing on job edit page if tracker job [sc-440]
    
    * Implement Filament Forms on job verify page [sc-440]
    
    * Remove old rules function from vacation request create page [sc-440]
    
    * Implement Filament Forms on screenshot create page [sc-440]
    
    * Implement Filament Forms on user edit page [sc-440]
    
    * Update AlpineJS [sc-440]
    
    * WIP [sc-440]
    
    * Implement Filament Forms on event create page [sc-440]
    
    * Hide announce toggle on event create page if publish toggle is false [sc-440]
    
    * Implement Filament Forms on event edit page [sc-440]
    
    * Implement Filament Forms on download create page [sc-440]
    
    * Implement Filament Forms on download edit page [sc-440]
    
    * Change which image types are supported in the screenshot create page [sc-440]
    
    * Remove old rules function from download edit page [sc-440]
    
    * Implement Filament Forms on cargo index/edit page [sc-440]
    
    * Implement Filament Forms on cargo index/edit page [sc-440]
    
    * Implement Filament Forms on city index/edit page [sc-440]
    
    * Implement Filament Forms on company index/edit page [sc-440]
    
    * Implement Filament Forms on game data request page [sc-440]

--- app/Http/Livewire/ScreenshotHub/ShowCreatePage.php
+++ app/Http/Livewire/ScreenshotHub/ShowCreatePage.php
@@ -4,29 +4,21 @@ namespace App\Http\Livewire\ScreenshotHub;
 
 use App\Models\Screenshot;
 use Carbon\Carbon;
+use Filament\Forms;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
-use Livewire\WithFileUploads;
 
-class ShowCreatePage extends Component
+class ShowCreatePage extends Component implements HasForms
 {
-    use WithFileUploads;
+    use InteractsWithForms;
 
     // Form fields
-    public string $title = '';
+    public $title;
     public $screenshot;
-    public string $description = '';
-    public string $location = '';
-
-    public function rules(): array
-    {
-        return [
-            'title' => ['required', 'string', 'max:30'],
-            'screenshot' => ['required', 'image', 'max:2048'],
-            'description' => ['present', 'string', 'max:100'],
-            'location' => ['present', 'string', 'max:30'],
-        ];
-    }
+    public $description;
+    public $location;
 
     public function mount()
     {
@@ -41,16 +33,6 @@ class ShowCreatePage extends Component
 
             return redirect()->route('screenshot-hub.index');
         }
-
-        session()->now('alert', [
-            'type' => 'info',
-            'title' => 'Please read this before submitting a screenshot!',
-            'message' => '
-                - You can submit a maximum of <b>one</b> screenshot every 24 hours.<br>
-                - Submissions needs to be yours. You cannot submit someone else\'s creation.<br>
-                - The screenshot must be taken in either Euro Truck Simulator 2 or American Truck Simulator.
-            '
-        ]);
     }
 
     public function render()
@@ -58,18 +40,64 @@ class ShowCreatePage extends Component
         return view('livewire.screenshot-hub.create-page')->extends('layouts.app');
     }
 
+    protected function getFormSchema(): array
+    {
+        return [
+            Forms\Components\Grid::make()
+                ->schema([
+                    Forms\Components\Grid::make()
+                        ->columns(1)
+                        ->schema([
+                            Forms\Components\TextInput::make('title')
+                                ->required()
+                                ->maxLength(30)
+                                ->placeholder('My awesome truck')
+                        ]),
+
+                    Forms\Components\Grid::make()
+                        ->columns(1)
+                        ->schema([
+                            Forms\Components\FileUpload::make('screenshot')
+                                ->disk('scaleway')
+                                ->directory('screenshot-hub-uploads')
+                                ->required()
+                                ->label('Screenshot')
+                                ->acceptedFileTypes(['image/jpeg', 'image/png'])
+                                ->hint('Max 2MB')
+                                ->maxSize(2048)
+                        ]),
+
+                    Forms\Components\Textarea::make('description')
+                        ->maxLength(100)
+                        ->rows(3)
+                        ->columnSpan([
+                            'sm' => 1,
+                            'md' => 2,
+                        ])
+                        ->placeholder('A small, optional description that describes or provides information about your screenshot. Max 100 characters.'),
+
+                    Forms\Components\Grid::make()
+                        ->columns(1)
+                        ->schema([
+                            Forms\Components\TextInput::make('location')
+                                ->maxLength(30)
+                                ->hint('Where the picture was taken, optional')
+                                ->placeholder('Amsterdam, the Netherlands')
+                        ]),
+                ]),
+        ];
+    }
+
     public function submit()
     {
-        $this->validate();
-
-        $image_path = $this->screenshot->storePublicly('screenshot-hub-uploads', 'scaleway');
+        $validatedData = $this->form->getState();
 
         $screenshot = Screenshot::create([
             'user_id' => Auth::id(),
-            'title' => $this->title,
-            'image_path' => $image_path,
-            'description' => $this->description,
-            'location' => $this->location,
+            'title' => $validatedData['title'],
+            'image_path' => $validatedData['screenshot'],
+            'description' => $validatedData['description'],
+            'location' => $validatedData['location'],
         ]);
 
         session()->flash('alert', ['type' => 'success', 'message' => 'Screenshot successfully submitted!']);
--- resources/views/livewire/jobs/submit/submit-page.blade.php
+++ resources/views/livewire/jobs/submit/submit-page.blade.php
@@ -8,124 +8,21 @@
 @endsection
 
 <div>
-    <form class="space-y-8 divide-y divide-gray-200" wire:submit.prevent="submit">
-        <div class="space-y-8 divide-y divide-gray-200">
-            <div>
-                <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                    <x-input.group label="Pickup City" :error="$errors->first('pickup_city')" col-span="3">
-                        <x-input.select2
-                            :url="route('api.game-data.cities', $game_id)"
-                            id="pickup_city"
-                            wire:model="pickup_city"
-                            placeholder="Select a city"/>
-                    </x-input.group>
+    <form wire:submit.prevent="submit">
+        <x-app-ui::card>
+            {{ $this->form }}
 
-                    <x-input.group label="Destination City" :error="$errors->first('destination_city')" col-span="3">
-                        <x-input.select2
-                            :url="route('api.game-data.cities', $game_id)"
-                            id="destination_city"
-                            wire:model="destination_city"
-                            placeholder="Select a city"/>
-                    </x-input.group>
+            <x-slot name="footer">
+                <x-app-ui::card.actions>
+                    <x-app-ui::button tag="a" href="{{ route('jobs.personal-overview') }}" color="secondary">
+                        Cancel
+                    </x-app-ui::button>
 
-                    <x-input.group label="Pickup Company" :error="$errors->first('pickup_company')" col-span="3">
-                        <x-input.select2
-                            :url="route('api.game-data.companies', $game_id)"
-                            id="pickup_company"
-                            wire:model="pickup_company"
-                            placeholder="Select a company"/>
-                    </x-input.group>
-
-                    <x-input.group label="Destination Company" :error="$errors->first('destination_company')" col-span="3">
-                        <x-input.select2
-                            :url="route('api.game-data.companies', $game_id)"
-                            id="destination_company"
-                            wire:model="destination_company"
-                            placeholder="Select a company"/>
-                    </x-input.group>
-
-                    <x-input.group label="Cargo" :error="$errors->first('cargo')" col-span="3">
-                        <x-input.select2
-                            :url="route('api.game-data.cargos', $game_id)"
-                            id="cargo"
-                            wire:model="cargo"
-                            placeholder="Select a cargo"/>
-                    </x-input.group>
-
-                    <br>
-
-                    <x-input.group label="Completed At" for="finished_at" :error="$errors->first('finished_at')"
-                                   col-span="3">
-                        <x-input.date id="finished_at" wire:model.lazy="finished_at"
-                                      :error="$errors->first('finished_at')"
-                                      :options="[
-                                        'enableTime' => false,
-                                        'altFormat' =>  'd/m/Y',
-                                        'minDate' => date('Y-m-d', strtotime('-7 days')),
-                                        'maxDate' => now()
-                                      ]"
-                                      trailing-icon="o-calendar"/>
-                    </x-input.group>
-
-                    <br>
-
-                    <x-input.group label="Distance" for="distance" :error="$errors->first('distance')" col-span="3"
-                                   help-text="Kilometres for ETS, miles for ATS">
-                        <x-input.text wire:model.lazy="distance" type="number" id="distance" min="1" max="5000"
-                                      leading-icon="o-location-marker"
-                                      :error="$errors->first('distance')" placeholder="1200"/>
-                    </x-input.group>
-
-                    <br>
-
-                    <x-input.group label="Cargo Damage" for="load_damage" :error="$errors->first('load_damage')"
-                                   col-span="3">
-                        <x-input.text wire:model.lazy="load_damage" type="number" id="load_damage" min="0" max="100"
-                                      leading-icon="o-shield-exclamation"
-                                      :error="$errors->first('load_damage')" placeholder="A value between 0 and 100%"/>
-                    </x-input.group>
-
-                    <br>
-
-                    <x-input.group label="Estimated Income" for="estimated_income"
-                                   :error="$errors->first('estimated_income')"
-                                   col-span="3">
-                        <x-input.text wire:model.lazy="estimated_income" type="number" id="estimated_income" min="1"
-                                      max="400000"
-                                      leading-icon="o-calculator"
-                                      :error="$errors->first('estimated_income')"
-                                      placeholder="The original estimate, before any penalties"/>
-                    </x-input.group>
-
-                    <x-input.group label="Total Income" for="total_income" :error="$errors->first('total_income')"
-                                   col-span="3">
-                        <x-input.text wire:model.lazy="total_income" type="number" id="total_income" min="1"
-                                      :max="$estimated_income"
-                                      leading-icon="o-currency-euro"
-                                      :error="$errors->first('total_income')"
-                                      placeholder="Including any in-game penalties"/>
-                    </x-input.group>
-
-                    <x-input.group label="Additional Comments" for="comments" :error="$errors->first('comments')">
-                        <x-input.textarea wire:model.lazy="comments" id="comments" rows="3"
-                                          :error="$errors->first('comments')"
-                                          placeholder="Any notes and/or comments about this delivery"/>
-                    </x-input.group>
-                </div>
-            </div>
-        </div>
-
-        <div class="pt-5">
-            <div class="flex justify-end">
-                <a href="{{ route('jobs.personal-overview') }}"
-                   class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Cancel
-                </a>
-                <button type="submit"
-                        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-600 text-base font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:ml-3 sm:w-auto sm:text-sm">
-                    Submit
-                </button>
-            </div>
-        </div>
+                    <x-app-ui::button type="submit">
+                        Submit
+                    </x-app-ui::button>
+                </x-app-ui::card.actions>
+            </x-slot>
+        </x-app-ui::card>
     </form>
 </div>
