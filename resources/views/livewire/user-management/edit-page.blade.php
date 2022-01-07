db3ca39d716be54fbe888dee2eee63d2b2c728e3
Mwikala Kangwa <hello@mwikala.co.uk>
2022-01-07 00:07:43 +0000

    Allow users to edit their slug

--- resources/views/livewire/user-management/edit-page.blade.php
+++ resources/views/livewire/user-management/edit-page.blade.php
@@ -5,8 +5,8 @@
 @section('actions')
     <div class="ml-3">
         <a href="{{ route('users.profile', $user) }}"
-           class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <x-heroicon-s-user class="-ml-1 mr-2 h-5 w-5 text-gray-500"/>
+           class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <x-heroicon-s-user class="w-5 h-5 mr-2 -ml-1 text-gray-500"/>
             View Profile
         </a>
     </div>
@@ -15,10 +15,10 @@
         <div class="relative inline-block text-left" x-data="{ open: false }">
             <div>
                 <button type="button"
-                        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-gray-600 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                        class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-gray-600 border border-gray-300 rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
                         id="menu-button" aria-expanded="true" aria-haspopup="true" @click="open = !open">
                     Profile Actions
-                    <x-heroicon-s-chevron-down class="-mr-1 ml-2 h-5 w-5"/>
+                    <x-heroicon-s-chevron-down class="w-5 h-5 ml-2 -mr-1"/>
                 </button>
             </div>
 
@@ -30,17 +30,17 @@
                  x-transition:leave="transition ease-in duration-75"
                  x-transition:leave-start="opacity-100 scale-100"
                  x-transition:leave-end="opacity-0 scale-95"
-                 class="origin-top-right absolute right-0 z-10 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                 class="absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                  role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div class="py-1" role="none">
                     <a href="{{ route('users.removeProfilePicture', $user) }}"
-                       class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900"
+                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                        role="menuitem" tabindex="-1">
                         Remove Profile Picture
                     </a>
 
                     <a href="{{ route('users.removeProfileBanner', $user) }}"
-                       class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900"
+                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                        role="menuitem" tabindex="-1">
                         Remove Profile Banner
                     </a>
@@ -61,7 +61,7 @@
 
             <x-slot name="footer">
                 <x-app-ui::card.actions>
-                    <x-app-ui::button tag="a" href="{{ route('users.profile', $user->id) }}" color="secondary">
+                    <x-app-ui::button tag="a" href="{{ route('users.profile', $user) }}" color="secondary">
                         Cancel
                     </x-app-ui::button>
 
