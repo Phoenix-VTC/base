4fb8c051c880f5f7e9e38726b03561e77ffcae12
Diego <me@diegor.nl>
2022-01-24 16:13:54 +0100

    Create a command to (re-)check all user levels [sc-405]

---
+++ app/Console/Commands/CheckUserLevels.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CheckUserLevel;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CheckUserLevels extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:check-levels';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dispatch a CheckUserLevel job for every user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            CheckUserLevel::dispatch($user);
+        }
+
+        $this->info('All level check jobs have been dispatched.');
+    }
+}
