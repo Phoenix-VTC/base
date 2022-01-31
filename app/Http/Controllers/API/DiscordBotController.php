99015b8baeadfa200c74d6c1d0c118a7cd6d82f5
Diego <me@diegor.nl>
2022-02-01 00:42:48 +0100

    Fix Discord bot API profile URL [sc-570] (#315)

--- app/Http/Controllers/API/DiscordBotController.php
+++ app/Http/Controllers/API/DiscordBotController.php
@@ -20,7 +20,7 @@ class DiscordBotController extends Controller
      */
     public function findUserByDiscordId($discordId): JsonResponse
     {
-        $user = User::select(['id', 'username', 'steam_id', 'truckersmp_id', 'discord->nickname as discord_nickname', 'profile_picture_path', 'driver_level', 'created_at'])
+        $user = User::select(['id', 'username', 'slug', 'steam_id', 'truckersmp_id', 'discord->nickname as discord_nickname', 'profile_picture_path', 'driver_level', 'created_at'])
             ->whereJsonContains('discord->id', $discordId)
             ->firstOrFail();
 
@@ -31,7 +31,7 @@ class DiscordBotController extends Controller
             'driver_points' => $user->totalDriverPoints(),
             'percentage_until_level_up' => $user->percentageUntilLevelUp(),
             'profile_picture' => $user->profile_picture,
-            'profile_link' => route('users.profile', $user->username)
+            'profile_link' => route('users.profile', $user->slug)
         ]);
 
         return response()->json($user);
