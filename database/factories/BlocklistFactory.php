ab70e6e9c20a34b420b085ec59f94c40b9937d09
Shift <shift@laravelshift.com>
2022-02-08 23:52:27 +0000

    Remove unnecessary `$model` property

--- database/factories/BlocklistFactory.php
+++ database/factories/BlocklistFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class BlocklistFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Blocklist::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -13,13 +13,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class JobFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Job::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
