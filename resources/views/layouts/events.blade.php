52600ac08e19a4a37717677249301791ebca599c
Diego <me@diegor.nl>
2022-02-16 12:26:03 +0100

    Replace GA with Plausible [sc-579]

--- resources/views/layouts/events.blade.php
+++ resources/views/layouts/events.blade.php
@@ -1,16 +1,6 @@
 @extends('layouts.base')
 
 @push('scripts')
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6GRVEBD26Y"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-6GRVEBD26Y');
-    </script>
-
     {{-- TEMP-WINTER --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Snowstorm/20131208/snowstorm-min.js"
             integrity="sha512-rMkLONrw50boYG/6Ku0E8VstfWMRn5D0dX3QZS26Mg0rspYq4EHxYOULuPbv9Be2HBbrrmN8dpPgYUeJ4bINCA=="
