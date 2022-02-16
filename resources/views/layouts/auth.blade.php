6eb37d00eb8142165a0d23e4482cb624b3a9d7f5
Diego <me@diegor.nl>
2022-02-16 12:56:55 +0100

    Remove snow

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,19 +1,5 @@
 @extends('layouts.base')
 
-{{-- TEMP-WINTER --}}
-@push('scripts')
-    @once
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Snowstorm/20131208/snowstorm-min.js"
-                integrity="sha512-rMkLONrw50boYG/6Ku0E8VstfWMRn5D0dX3QZS26Mg0rspYq4EHxYOULuPbv9Be2HBbrrmN8dpPgYUeJ4bINCA=="
-                crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    @endonce
-    <script>
-        snowStorm.targetElement = 'cover-image'; // Render it in the cover-image div
-        snowStorm.followMouse = false; // Snowflakes won't follow the mouse
-        snowStorm.animationInterval = 100; // Set the interval for the snow to fall (lower value = more snow)
-    </script>
-@endpush
-
 @section('body')
     <div class="min-h-screen bg-white flex">
         <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
